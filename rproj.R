517a38a18288c41274215cf6363387df4c05365b
srtaheri <srtaheri66@gmail.com>
2018-05-02 17:18:23 -0400

    boxplot sepal length

--- rproj.R
+++ rproj.R
@@ -3,3 +3,4 @@ plot(x = iris$Sepal.Length, y = iris$Sepal.Width, xlab = "sepal length",
      ylab = "sepal width", main = "scatter plot")
 hist(x = iris$Petal.Length, xlab = "petal length")
 hist(x = iris$Sepal.Width, xlab = "Sepal width"")
+boxplot(x = iris$Sepal.Length) 
