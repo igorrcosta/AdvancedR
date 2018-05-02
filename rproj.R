e1466d2d2d1c2b737fe60a5a30786cc4e66a278e
srtaheri <srtaheri66@gmail.com>
2018-05-02 17:13:52 -0400

    hist iris petal length

--- rproj.R
+++ rproj.R
@@ -1,3 +1,4 @@
 head(iris)
 plot(x = iris$Sepal.Length, y = iris$Sepal.Width, xlab = "sepal length",
      ylab = "sepal width", main = "scatter plot")
+hist(x = iris$Petal.Length, xlab = "petal length”)
