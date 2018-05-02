d3fa266c64086c1766cf841adec026474eef48e0
srtaheri <srtaheri66@gmail.com>
2018-05-02 17:25:53 -0400

    complex function

--- rproj.R
+++ rproj.R
@@ -6,4 +6,7 @@ hist(x = iris$Sepal.Width, xlab = "Sepal width")
 boxplot(x = iris$Petal.Width)
 hist(x = iris$Sepal.Width, xlab = "Sepal width")
 boxplot(x = iris$Sepal.Length) 
-
+complex_func <- function(x,y,z) {
+  return(sum(sqrt(x + y) + z))
+}
+print(complex_func(x = iris$Sepal.Length,y=iris$Sepal.Width,z=iris$Petal.Length))
